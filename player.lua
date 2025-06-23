85f7a67b464b8b45b1938b2297722dd18040c1a2
Prajit P <prajitpradeep@gmail.com>
2025-06-23 23:28:35 +0530

    minor code cleanup

--- player.lua
+++ player.lua
@@ -21,6 +21,75 @@ end
 
 --Player:update(dt) == Player.update(self,dt)
 function Player:update(dt,entities,items)
+    self:handleMovement(dt)
+    if self:canAttack() then
+        self.swingTimer = self.swingDuration
+    end
+
+    if(self.swingTimer > 0) then
+        self.swingTimer = self.swingTimer - dt
+        self:attemptSlash(entities,items)
+    end
+
+    if love.keyboard.wasPressed and love.keyboard.wasPressed["e"] then
+        self:pickupAndDrop(items)
+    end
+end
+
+function Player:pickupAndDrop(items)
+    if self.holdingItem then
+        self.holdingItem.x = self.x + self.w/2 - self.holdingItem.w/2
+        self.holdingItem.y = self.y + self.h/2 - self.holdingItem.h/2
+        self.holdingItem.picked = false
+        self.holdingItem = nil
+    else
+        local px,py = self.x + self.w/2,self.y + self.h/2
+        for _,item in ipairs(items) do
+            if not item.picked then
+                local ix,iy = item.x + item.w/2, item.y + item.h/2
+                if math.sqrt((px-ix)^2 + (py-iy)^2) < 30 then
+                    item.picked = true
+                    self.holdingItem = item
+                    break
+                end
+            end
+        end
+    end
+    
+end
+
+function Player:attemptSlash(entities,items)
+    for _, e in ipairs(entities) do
+        if not e.harvested then
+            local ex = e.x + e.w / 2
+            local ey = e.y + e.h / 2
+            local px = self.x + self.w / 2
+            local py = self.y + self.h / 2
+            local dx = ex - px
+            local dy = ey - py
+            local dist = math.sqrt(dx * dx + dy * dy)
+
+            if dist < self.attackRadius then
+                local angleToEntity = math.atan2(dy, dx)
+                local diff = math.abs(angleToEntity - self.angle)
+
+                if diff > math.pi then
+                    diff = 2 * math.pi - diff
+                end
+
+                if diff < math.rad(30) then
+                    e.harvested = true
+                    -- self.oil = self.oil + 1
+                    local item = Item.new(e.x + e.w/2,e.y + e.h/2)
+                    table.insert(items,item)
+                end
+            end
+        end
+    end
+    
+end
+
+function Player:handleMovement(dt)
     local dx,dy =0,0
     if love.keyboard.isDown("w") then
         dy = dy-1
@@ -44,64 +113,11 @@ function Player:update(dt,entities,items)
     local mx,my = love.mouse.getPosition()
     local px,py = self.x + self.w/2, self.y + self.h/2
     self.angle = math.atan2(my-py,mx-px)
+end
 
-    if love.mouse.isDown(1) and self.swingTimer <= 0 and not self.holdingItem then
-        self.swingTimer = self.swingDuration
-    end
-
-    if(self.swingTimer > 0) then
-        self.swingTimer = self.swingTimer - dt
-        for _, e in ipairs(entities) do
-            if not e.harvested then
-                local ex = e.x + e.w / 2
-                local ey = e.y + e.h / 2
-                local px = self.x + self.w / 2
-                local py = self.y + self.h / 2
-                local dx = ex - px
-                local dy = ey - py
-                local dist = math.sqrt(dx * dx + dy * dy)
-
-                if dist < self.attackRadius then
-                    local angleToEntity = math.atan2(dy, dx)
-                    local diff = math.abs(angleToEntity - self.angle)
-
-                    if diff > math.pi then
-                        diff = 2 * math.pi - diff
-                    end
-
-                    if diff < math.rad(30) then
-                        e.harvested = true
-                        -- self.oil = self.oil + 1
-                        local item = Item.new(e.x + e.w/2,e.y + e.h/2)
-                        table.insert(items,item)
-                    end
-                end
-            end
-        end
-    end
-
-    if love.keyboard.wasPressed and love.keyboard.wasPressed["e"] then
-        if self.holdingItem then
-            self.holdingItem.x = self.x + self.w/2 - self.holdingItem.w/2
-            self.holdingItem.y = self.y + self.h/2 - self.holdingItem.h/2
-            self.holdingItem.picked = false
-            self.holdingItem = nil
-        else
-            local px,py = self.x + self.w/2,self.y + self.h/2
-            for _,item in ipairs(items) do
-                if not item.picked then
-                    local ix,iy = item.x + item.w/2, item.y + item.h/2
-                    if math.sqrt((px-ix)^2 + (py-iy)^2) < 30 then
-                        item.picked = true
-                        self.holdingItem = item
-                        break
-                    end
-                end
-            end
-        end
-    end
-
-end  
+function Player:canAttack()
+    return love.mouse.isDown(1) and self.swingTimer <= 0 and not self.holdingItem
+end
 
 function Player:draw()
     love.graphics.setColor(1,1,0,1)
